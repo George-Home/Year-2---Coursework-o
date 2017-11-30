6caba33f5df2d830e585938f4be23227b6f5c1c6
GodofCheez <georgefball@googlemail.com>
2017-11-30 19:00:02 +0000

    Update Week 1 - Task 1 - Basic.py

--- Week 1 - Task 1 - Basic.py
+++ Week 1 - Task 1 - Basic.py
@@ -1,7 +1,14 @@
 #combining strings :o
-def string_join ():
+def string_join (Str_1,Str_2):
+   
     position = 0                #keeps track of the pointer along the string
     Str_3 = ""                  #placeholder for end result
+
+    if isinstance (Str_1, str) and isinstance (Str_2, str):
+        pass
+    else:
+        print("Please enter appropriate string values")
+    
 
     #checks which string is longer and saves the extra characters to its own variable
     if len(Str_1) > len(Str_2):
@@ -17,12 +24,15 @@ def string_join ():
         Str_3 = (Str_3 + Str_1[position] + Str_2[position])
         
         position = position + 1
-    #prints the end result    
-    print(Str_3 + extra)        
+                            #prints the end result    
+    #print(Str_3 + extra)
+    output = Str_3 + extra
+    return output
 
 
 #input for both strings
-Str_1 = str(input("Enter the first string: "))
-Str_2 = str(input("Enter the second string: "))
+#Str_1 = str(input("Enter the first string: "))         #removed for unit testing
+#Str_2 = str(input("Enter the second string: "))
 #runs the string combine function
-string_join () 
+#string_join (Str_1,Str_2) 
+
