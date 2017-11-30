143c41357eda3d180a43c07aff2b7a785009d8c6
George Ball <georgefball@googlemail.com>
2017-11-30 22:36:42 +0000

    Update Week 3 - Task 2 - Basic.py

--- Week 3 - Task 2 - Basic.py
+++ Week 3 - Task 2 - Basic.py
@@ -1,25 +1,19 @@
-def linear():                                                           #performes a linear search on a given list
+def linear(Target):                                                           #performes a linear search on a given list
     list = [3, 7, 2, 9, 4, 23, 45, 76, 45, 46, 56, 6, 4, 34, 5, 56]     #the given list
     length = len(list)
     position = 0
     
 
+    try:
 
-    while position <= length -1:                                        #compares target value with values in list to find a match
-        if list[position] == Target:
-            print("Found")
-            return
-        if position == length -1:
-            print("Not Found")
-            return
-            
-        else:
-            position += 1
+        while position <= length -1:                                        #compares target value with values in list to find a match
+            if list[position] == Target:
+                return True
+            if position == length -1:
+                return False   
+            else:
+                position += 1
 
+    except:
+        print("Target is invalid")              #input validation
 
-
-
-Target = int(input("Enter the target: "))
-linear()
-
-###Time Complexity O(n)###
