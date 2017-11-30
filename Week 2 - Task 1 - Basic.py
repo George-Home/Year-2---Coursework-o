270bef52cb13168d97d3024ee5aaa03fc16d1190
GodofCheez <georgefball@googlemail.com>
2017-11-30 19:24:19 +0000

    Update Week 2 - Task 1 - Basic.py

--- Week 2 - Task 1 - Basic.py
+++ Week 2 - Task 1 - Basic.py
@@ -1,15 +1,16 @@
-def m_Poly():
+def m_Poly(P1,P2,Degree1,Degree2):
 
-
-    degree = 2 + 3      #degree 1 , degree two 
-    P1 = [3,1,9]
-    P2 = [2,1,2,1]
-    res = [0]*(degree+1)    #both degrees added then add one for size of result list
+    try:
+        degree = Degree1 + Degree2      #degree 1 , degree two 
+    #P1 = [3,1,9]
+    #P2 = [2,1,2,1]
+        res = [0]*(degree+1)    #both degrees added then add one for size of result list
                             #is also initialised as all zeros
-    for i in range(0,2 + 1):                #performs multiplication of polynomials
-        for j in range(0,3 + 1):
-            res[i+j] += P1[i] * P2[j]
+        for i in range(0,2 + 1):                #performs multiplication of polynomials
+            for j in range(0,3 + 1):
+                res[i+j] += P1[i] * P2[j]
 
-    print(res)
+        return list(res)
 
-m_Poly()
+    except:
+        print("please enter correct input values")      #input validation
