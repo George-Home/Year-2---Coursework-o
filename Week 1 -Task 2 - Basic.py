f5c6055da88cc6051b8e003b61a3c976df81d443
GodofCheez <georgefball@googlemail.com>
2017-11-30 19:13:29 +0000

    Update Week 1 -Task 2 - Basic.py

--- Week 1 -Task 2 - Basic.py
+++ Week 1 -Task 2 - Basic.py
@@ -1,24 +1,25 @@
-def Armstrong_Chk ():   #function to check if a given number is also an armstrong number
+def Armstrong_Chk (user_number):   #function to check if a given number is also an armstrong number
     
-    Total = 0           
+    Total = 0
+
+    try:
     
-    user_number = int(input("Enter your number: "))     #requests a number to test from the user
-    number_s = str(user_number)                         #converts given number into a string
-    length_num = len(number_s)                          #finds the length of the given number
+    #user_number = int(input("Enter your number: "))     #requests a number to test from the user           #removed for unit test
+        number_s = str(user_number)                         #converts given number into a string
+        length_num = len(number_s)                          #finds the length of the given number
 
-    for i in number_s:                                  #finds the sum of the cubes of its digits
-        Total += int(i) ** length_num
+        for i in number_s:                                  #finds the sum of the cubes of its digits
+            Total += int(i) ** length_num
 
-    if user_number == Total:                            #prints output if the number is an armstrong number or not
-        print(number_s + " is a armstrong number... yay")
-    else:
-        print(number_s + " is not an armstrong number")
+        if user_number == Total:                            #prints output if the number is an armstrong number or not
+        #print(number_s + " is a armstrong number... yay")
+            output1 = True
+            return output1
+        else:
+        #print(number_s + " is not an armstrong number")
+            return output1
 
-    
+    except:
+        print("Enter an appropriate number")        #input validation
 
-
-
-Armstrong_Chk()
-
-
-###############testing notes - write small algorithm to count to a given number and output any armstrong numbers
+#Armstrong_Chk(user_number)
