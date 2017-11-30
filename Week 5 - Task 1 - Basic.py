f580fbc52a6fea6e0e7b650159f5d8c06dbd611d
George Ball <georgefball@googlemail.com>
2017-11-30 22:42:32 +0000

    Update Week 5 - Task 1 - Basic.py

--- Week 5 - Task 1 - Basic.py
+++ Week 5 - Task 1 - Basic.py
@@ -27,14 +27,16 @@ class List(object):
           while n!=None:
               values.append(str(n.value))
               n=n.next  
-          print ("List: ",",".join(values))
+          #print ("List: ",",".join(values))                            #removed for unit test
+
+          return values
 
       def duplicate(self):
             n = dupe = self.head                            #assigns the checkers to the start of the lists
             while n is not None:                            #checks that the current node has a value
                   while dupe.next is not None:              #checks if the next node has a value
                         if dupe.next.value == n.value:
-                              dupe.next = dupe.next.next    #if they match, the duplicate is assigned as 'none' and thus removed
+                              dupe.next = dupe.next.next    #if they match, the duplicate is removed
                         else:
                               dupe = dupe.next
                   n = dupe = n.next
@@ -44,6 +46,7 @@ if __name__ == '__main__':
       l.insert(None, Node(4))
       l.insert(l.head,Node(6))
       l.insert(l.head,Node(8))
-      l.insert(l.head,Node(8))                              #duplicate node added for testing
-      l.duplicate()                                         #runs the duplicate remover before displaying the results and after node values have been inserted
+      l.insert(l.head,Node(8))      #duplicate node added for testing  
+      l.duplicate()                 #runs the duplicate remover before displaying the results and after node values have been inserted
       l.display()
+
