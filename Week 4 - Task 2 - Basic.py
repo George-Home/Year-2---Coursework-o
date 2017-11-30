c7de429f2d08f2e79f237eb61b6a4ea40abb6601
George Ball <georgefball@googlemail.com>
2017-11-30 22:40:30 +0000

    Update Week 4 - Task 2 - Basic.py

--- Week 4 - Task 2 - Basic.py
+++ Week 4 - Task 2 - Basic.py
@@ -3,23 +3,28 @@ def binarySearch(value):
     midValue = 10000
     minValue = 0
     found = False
+
     while found == False:
-        midValue = int(((maxValue - minValue) / 2) + minValue)                                      #calculates the current middle value
-        if midValue == value:                                                                       #checks if it has found your value
-            found == True
-            print("Guessed it!")
-            break
-        check = input("Is your answer {}? Press h for higher or l for lower: " .format(midValue))     
-        if check == "h":                                                                            #if value is higher - the minimum is adjusted to remove null values
-            minValue = midValue
-        elif check == "l":                                                                          #if value is lower - the maximum is adjusted to remove null values
-            maxValue = midValue
-        else:
-            print("This is an incorrect value") 
-                
+        try:
+            if value > maxValue or value < minValue:            #input validation
+                print("value given is out of range")
+                break
+            midValue = int(((maxValue - minValue) / 2) + minValue)                                      #calculates the current middle value
+            if midValue == value:                                                                       #checks if it has found your value
+                found == True
+                print("Guessed it!")
+                return True
+                break
+            check = input("Is your answer {}? Press h for higher or l for lower: " .format(midValue))     
+            if check == "h":                                                                            #if value is higher - the minimum is adjusted to remove null values
+                minValue = midValue
+            elif check == "l":                                                                          #if value is lower - the maximum is adjusted to remove null values
+                maxValue = midValue
+            else:
+                print("This is an incorrect value")         #input validation
+        except:
+            print("the value given is incorrect")
 
 
-
-
-value = int(input("Enter a number between 1 and 20000: "))
-binarySearch(value) 
+#value = int(input("Enter a number between 1 and 20000: "))
+#binarySearch(value) 
